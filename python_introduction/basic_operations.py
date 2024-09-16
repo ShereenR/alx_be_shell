7531f62918d6953c2804254c74dbbbe6ce3432d4
sheren reda <sheryreda557@gmail.com>
2024-09-16 07:20:36 +0300

    create first file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+print(f"Addition of {number1} and {number2} is 15")
+print(f"Subtraction of {number1} and {number2} is 5")
+print(f"Multiplication of {number1} and {number2} is 50")
