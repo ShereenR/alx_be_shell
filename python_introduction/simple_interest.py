c6db5007c3bea5aef7aa1550e51d5fdc3a14c995
sheren reda <sheryreda557@gmail.com>
2024-09-16 08:18:31 +0300

    create seconde file

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,5 @@
 principal = 1000 
 rate = 0.05
 time = 3
-interest =print(principal * rate * time)
-print(f"The simple interest is 150.0")
+interest = principal * rate * time
+print(f"The simple interest is  {interest}")
